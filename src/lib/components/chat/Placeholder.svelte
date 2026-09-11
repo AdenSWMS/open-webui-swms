e1a6cbe06de20cdc6b356287f67afab25d9fe206
Simon Aden <simonadn168@gmail.com>
2026-09-12 00:12:22 +0200

    reduced again

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -249,7 +249,7 @@
 				</div>
 			</div>
 
-			<div class="flex mt-1 mb-8">
+			<div class="flex mt-1 mb-5">
 				<div in:fade={{ duration: 100, delay: 50 }}>
 					{#if models[selectedModelIdx]?.info?.meta?.description ?? null}
 						<Tooltip
